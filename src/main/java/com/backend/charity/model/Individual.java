35bdf862145e65f3209daa62661d9a4ba8f44a44
pinkpegasus <rukiyadykes@gmail.com>
2022-02-25 09:53:21 -0600

    individual: variables, constructors, getters + setters

--- src/main/java/com/backend/charity/model/Individual.java
+++ src/main/java/com/backend/charity/model/Individual.java
@@ -1,4 +1,83 @@
 package com.backend.charity.model;
 
 public class Individual {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String size;
+    private Long age;
+
+    public Individual() {
+    }
+
+    public Individual(Long id, String firstName, String lastName, String gender, String size, Long age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.size = size;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Individual{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", size='" + size + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }

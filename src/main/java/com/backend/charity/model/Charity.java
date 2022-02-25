e9a7504bdc8ba8ad82c9abfb904972528fa48645
pinkpegasus <rukiyadykes@gmail.com>
2022-02-25 09:45:24 -0600

    charity: variables, constructors, getters + setters

--- src/main/java/com/backend/charity/model/Charity.java
+++ src/main/java/com/backend/charity/model/Charity.java
@@ -1,4 +1,94 @@
 package com.backend.charity.model;
 
 public class Charity {
+
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private Long phoneNumber;
+    private String website;
+
+    public Charity() {
+    }
+
+    public Charity(Long id, String name, String address, String city, String state, Long phoneNumber, String website) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "Charity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", website='" + website + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 }
